5177c6696fa8a247a473f7b9d48c450214d17776
laetitialeprael <76735358+laetitialeprael@users.noreply.github.com>
2021-05-03 13:52:24 +0200

    Connection to the database

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,14 @@
+<?php
+/*
+ * Connexion à la BDD
+ * On instancie la class PDO que l'on stock dans la variable $pdo
+ * @var $pdo
+*/
+$pdo = new PDO('mysql:dbname=openclassroomsblog;host=localhost', "root", "root");
+// Affiche le nombre de résulatat affecté
+$count = $pdo->exec('INSERT INTO post SET title="Mon titre", date="' . date('Y-m-d H:i:s') . '"');
+var_dump($count);
+?>
 <h1>Page d'accueil</h1>
 <p>On affiche quelques articles du blog</p>
 <p><a href="index.php?page=blog">Voir tous les articles</a></p>
