bf6231796593dfda884c8b276d26de4d28a88eff
laetitialeprael <76735358+laetitialeprael@users.noreply.github.com>
2021-05-03 13:57:48 +0200

    Show sql errors

--- pages/home.php
+++ pages/home.php
@@ -5,9 +5,8 @@
  * @var $pdo
 */
 $pdo = new PDO('mysql:dbname=openclassroomsblog;host=localhost', "root", "root");
-// Affiche le nombre de résulatat affecté
-$count = $pdo->exec('INSERT INTO post SET title="Mon titre", date="' . date('Y-m-d H:i:s') . '"');
-var_dump($count);
+// Afficher les erreurs sql
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
 <h1>Page d'accueil</h1>
 <p>On affiche quelques articles du blog</p>
