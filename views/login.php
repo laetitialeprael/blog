272879d525fb1d46abde31c5a19b98b68770bb04
laetitialeprael <76735358+laetitialeprael@users.noreply.github.com>
2021-05-11 10:44:53 +0200

    Creation of the login form

--- views/login.php
+++ views/login.php
@@ -1,1 +1,11 @@
 <h1>Formulaire de connexion</h1>
+
+<form method="post" action="">
+	<label for="inputEmailAdress">Votre email</label>
+	<input required name="user_email" type="email" id="inputEmailAdress">
+
+	<label for="inputPassword">Votre mot de passe</label>
+	<input required name="user_password" type="password" id="inputPassword">
+
+	<button type="submit">Connexion</button>
+</form>
