de51a1aa8e59a9c3f872890353a2297614858ba8
laetitialeprael <76735358+laetitialeprael@users.noreply.github.com>
2021-05-25 18:49:47 +0200

    Update account.php

--- views/account.php
+++ views/account.php
@@ -1,5 +1,6 @@
 <h1>Profil de l'utilisateur</h1>
-
+<p>Date de création du compte :<?php echo $_SESSION['creationDate']; ?></p>
+<p>Date de la dernière visite :<?php echo $_SESSION['lastVisit']; ?></p>
 <p>Nom : <?php echo $_SESSION['name']; ?></p>
 <p>Prénom : <?php echo $_SESSION['firstname']; ?></p>
 <p>Email : <?php echo $_SESSION['email']; ?></p>
