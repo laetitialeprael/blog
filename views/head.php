d351c65775e837fea86bc0163b04778a0c493d5e
laetitialeprael <76735358+laetitialeprael@users.noreply.github.com>
2021-06-07 13:46:37 +0200

    Modified htaccess file to support css files

---
+++ views/head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+    	<meta charset="utf-8">
+        <title>Titre de la page</title>
+        <link rel="stylesheet" href="/blog/vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/blog/vendor/components/font-awesome/css/all.min.css">
+        <link rel="stylesheet" href="/blog/public/css/style.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com"> 
+		<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@700&display=swap" rel="stylesheet">
+    </head>
--- views/header.php
+++ views/header.php
@@ -1,7 +1,38 @@
-<h1>Header</h1>
-<?php if(empty($_SESSION['user']['iduser'])): ?>
-	<a href="../blog/connexion">Connexion</a>
-	<a href="../blog/creer-un-compte">Créer un compte</a>
-<?php else: ?>
-	<a href="">Déconnexion</a>
-<?php endif ?>
+<header class="navbar-white">
+	<div class="navbar-top d-none d-lg-block bg-light">
+		<div class="container">
+			<div class="d-md-flex justify-content-between align-items-center">
+				<div class="d-flex align-items-center justify-content-center">
+					<ul class="nav justify-content-center justify-content-md-start">
+						<li class="nav-item me-3">
+							<a class="navbar-brand text-dark" href="/openclassroomsblog/">OpenclassroomsBlog</a>
+						</li>
+					</ul>
+				</div>
+				<div class="d-flex align-items-center justify-content-center">
+					<ul class="nav ms-3">
+						<li class="nav-item">
+							<?php if(empty($_SESSION['user']['iduser'])): ?>
+								<a class="nav-link current-text text-dark" href="../blog/connexion"><i class="far fa-user me-2 text-mustard"></i>Connexion</a>
+								<a class="nav-link current-text text-dark" href="../blog/creer-un-compte">Créer un compte</a>
+							<?php else: ?>
+								<a href="">Mon compte</a>
+								<a href="">Déconnexion</a>
+							<?php endif ?>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="divider-light opacity-1"></div>
+	</div>
+	<nav class="navbar navbar-expand-lg">
+		<div class="container">
+			<ul class="navbar-nav navbar-nav-scroll navbar-nav-scroll mx-auto">
+				<li class="nav-item"><a class="nav-link text-dark" href="/openclassroomsblog/">Accueil</a></li>
+				<li class="nav-item"><a class="nav-link text-dark" href="/openclassroomsblog/blog">Blog</a></li>
+				<li class="nav-item"><a class="nav-link text-dark" href="/openclassroomsblog/contact">Contact</a></li>
+			</ul>
+		</div>
+	</nav>
+</header>
