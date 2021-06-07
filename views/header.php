6f7c21411ae6b09696fb4d9ca7cff8ce85c45466
laetitialeprael <76735358+laetitialeprael@users.noreply.github.com>
2021-06-07 14:00:19 +0200

    Editing navigation links

--- views/header.php
+++ views/header.php
@@ -5,7 +5,7 @@
 				<div class="d-flex align-items-center justify-content-center">
 					<ul class="nav justify-content-center justify-content-md-start">
 						<li class="nav-item me-3">
-							<a class="navbar-brand text-dark" href="/openclassroomsblog/">OpenclassroomsBlog</a>
+							<a class="navbar-brand text-dark" href="/blog/">OpenclassroomsBlog</a>
 						</li>
 					</ul>
 				</div>
@@ -29,9 +29,9 @@
 	<nav class="navbar navbar-expand-lg">
 		<div class="container">
 			<ul class="navbar-nav navbar-nav-scroll navbar-nav-scroll mx-auto">
-				<li class="nav-item"><a class="nav-link text-dark" href="/openclassroomsblog/">Accueil</a></li>
-				<li class="nav-item"><a class="nav-link text-dark" href="/openclassroomsblog/blog">Blog</a></li>
-				<li class="nav-item"><a class="nav-link text-dark" href="/openclassroomsblog/contact">Contact</a></li>
+				<li class="nav-item"><a class="nav-link text-dark" href="/blog/">Accueil</a></li>
+				<li class="nav-item"><a class="nav-link text-dark" href="/blog/article">Blog</a></li>
+				<li class="nav-item"><a class="nav-link text-dark" href="/blog/contact">Contact</a></li>
 			</ul>
 		</div>
 	</nav>
