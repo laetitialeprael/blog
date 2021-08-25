c724d51762eed5f7d916a22d00426fd09fb5fac3
laetitialeprael <76735358+laetitialeprael@users.noreply.github.com>
2021-08-25 10:37:16 +0200

    Update UserManager.php
    Code cleanup

--- src/Models/UserManager.php
+++ src/Models/UserManager.php
@@ -1,31 +1,39 @@
 <?php
-/*
- * Créer la class global Manager qui se chargera de se connecter à la db
- * Vérifier si c'est le controller ou le model qui se connecte.
-*/
 
 namespace Src\Models;
 
 use Src\Database;
 
-/*
+/**
  * Class UserManager
  *
  * @package Src
-*/
+ */
 class UserManager extends Manager
 {
+    /**
+     * Méthode pour enregistrer un utilisateur
+     * 
+     * @param string $name
+     * @param string $firstname
+     * @param string $email
+     * 
+     * @return void
+     */
     public function create($name, $firstname, $email)
     {
         $db = $this->getDatabase();
         $user = $db->insert(
-        //$statement
-        'INSERT INTO user (user_name, user_first_name, user_email) VALUES (:user_name, :user_first_name, :user_email)',
-        //$attributes
-        [':user_name' => $name,':user_first_name' => $firstname,':user_email' => $email]
+            'INSERT INTO user (user_name, user_first_name, user_email) VALUES (:user_name, :user_first_name, :user_email)',
+            [':user_name' => $name,':user_first_name' => $firstname,':user_email' => $email]
         );
     }
 
+    /**
+     * Méthode pour afficher les données de l'utilisateur
+     * 
+     * @return array $user
+     */
     public function read()
     {
         $db = $this->getDatabase();
@@ -33,32 +41,47 @@ class UserManager extends Manager
         return $user;
     }
 
+    /**
+     * Méthode pour enregistrer le mot de passe de l'utilisateur
+     * 
+     * @param string $password
+     * @param string $email
+     * 
+     * @return void
+     */
     public function createPassword($password, $email)
     {
         $db = $this->getDatabase();
         $user = $db->insert(
-        //$statement
-        'UPDATE user SET user_password = :user_password, role = "2" WHERE user.user_email = :user_email',
-        //$attributes
-        [':user_password' => $password, ':user_email' => $email]
+            'UPDATE user SET user_password = :user_password, role = "2" WHERE user.user_email = :user_email',
+            [':user_password' => $password, ':user_email' => $email]
         );
     }
 
+    /**
+     * Méthode pour mettre à jour le mot de passe de l'utilisateur
+     * 
+     * @param string $password
+     * @param string $email
+     * 
+     * @return void
+     */
     public function updatePassword($password, $email)
     {
         $db = $this->getDatabase();
         $user = $db->insert(
-        //$statement
-        'UPDATE user SET user_password = :user_password WHERE user.user_email = :user_email',
-        //$attributes
-        [':user_password' => $password, ':user_email' => $email]
+            'UPDATE user SET user_password = :user_password WHERE user.user_email = :user_email',
+            [':user_password' => $password, ':user_email' => $email]
         );
     }
 
-    public function delete()
-    {
-    }
-
+    /**
+     * Méthode pour afficher les données de l'utilisateur
+     * 
+     * @param string $email
+     * 
+     * @return User
+     */
     public function connexion($email)
     {
         $db = $this->getDatabase();
