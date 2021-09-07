d58b8bf2e5a24fdf9bf9a91727c4badbaeef7be1
laetitialeprael <76735358+laetitialeprael@users.noreply.github.com>
2021-09-07 14:14:06 +0200

    Update home.php

--- views/home.php
+++ views/home.php
@@ -19,20 +19,20 @@
 			<?php foreach ($result as $post): ?>
 			<div class="col-md-6 col-lg-4">
 				<div class="card shadow-sm p-3 h-100 overflow-hidden">
-						<div class="card-body d-flex align-items-start flex-column">
-							<div class="mb-auto">
-								<div class="card-meta mb-3 d-flex flex-row align-items-center text-gray current-text">
-									<span class="text-capitalize"><?php echo $post->getCategory(); ?></span>
-								</div>
-								<h5><?php echo $post->getTitle(); ?></h5>
-								<p class="mb-0">Par <?php echo $post->viewAuthor(); ?></p>
-								<p class="small mt-n1 mb-0"><?php echo $post->getCreationDate(); ?></p>
-								<p class="text-gray current-text"><?php echo $post->viewExtract(); ?></p>
+					<div class="card-body d-flex align-items-start flex-column">
+						<div class="mb-auto">
+							<div class="card-meta mb-3 d-flex flex-row align-items-center text-gray current-text">
+								<span class="text-capitalize"><?php echo $post->getCategory(); ?></span>
 							</div>
-							<div class="mt-auto">
-								<a class="btn btn-primary rounded-0 position-relative" href="#">Lire la suite</a>
-							</div>
+							<h5><?php echo $post->getTitle(); ?></h5>
+							<p class="mb-0">Par <?php echo $post->viewAuthor(); ?></p>
+							<p class="small mt-n1 mb-0"><?php echo $post->getCreationDate(); ?></p>
+							<p class="text-gray current-text"><?php echo $post->viewExtract(); ?></p>
 						</div>
+						<div class="mt-auto">
+							<a class="btn btn-primary rounded-0 position-relative" href="#">Lire la suite</a>
+						</div>
+					</div>
 				</div>
 			</div>
 			<?php endforeach; ?>
