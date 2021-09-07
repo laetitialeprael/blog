91e0191545e398fc23193c3ef84d342eb602f8fe
laetitialeprael <76735358+laetitialeprael@users.noreply.github.com>
2021-09-07 14:13:40 +0200

    Update archive.php

--- views/archive.php
+++ views/archive.php
@@ -3,31 +3,24 @@
 		<div class="row">
 			<h2 class="text-center mb-4">Les articles du blog</h2>
 			<?php foreach ($result as $post): ?>
-				<div class="col-md-6 col-lg-4">
-					<div class="card rounded-0 border-0 h-100 overflow-hidden">
-						<div class="card-img position-relative card-element-hover">
-							<img src="/blog/public/images/exemple-blog.jpg">
-							<div class="position-absolute top-0 m-4 d-flex">
-								<div class="ms-3 my-2 text-start">
-									<p class="mb-0 text-white">Par <?php echo $post->viewAuthor(); ?></p>
-									<p class="small mt-n1 mb-0 text-white"><?php echo $post->getCreationDate(); ?></p>
-								</div>
+				<div class="col-md-6 col-lg-4 py-3">
+				<div class="card shadow-sm p-3 h-100 overflow-hidden">
+					<div class="card-body d-flex align-items-start flex-column">
+						<div class="mb-auto">
+							<div class="card-meta mb-3 d-flex flex-row align-items-center text-gray current-text">
+								<span class="text-capitalize"><?php echo $post->getCategory(); ?></span>
 							</div>
+							<h5><?php echo $post->getTitle(); ?></h5>
+							<p class="mb-0">Par <?php echo $post->viewAuthor(); ?></p>
+							<p class="small mt-n1 mb-0"><?php echo $post->getCreationDate(); ?></p>
+							<p class="text-gray current-text"><?php echo $post->viewExtract(); ?></p>
 						</div>
-						<div class="card-body py-4 px-0 d-flex align-items-start flex-column">
-							<div class="mb-auto">
-								<div class="card-meta mb-3 d-flex flex-row align-items-center text-gray current-text">
-									<span class="text-capitalize"><?php echo $post->getCategory(); ?></span>
-								</div>
-								<h5><?php echo $post->getTitle(); ?></h5>
-								<p class="text-gray current-text"><?php echo $post->viewExtract(); ?></p>
-							</div>
-							<div class="mt-auto">
-								<a class="btn text-dark btn-sm btn-line position-relative" href="/blog/article/<?php echo $post->viewUrl(); ?>">Lire la suite</a>
-							</div>
+						<div class="mt-auto">
+							<a class="btn btn-primary rounded-0 position-relative" href="#">Lire la suite</a>
 						</div>
 					</div>
 				</div>
+			</div>
 			<?php endforeach; ?>
 		</div>
 	</div>
