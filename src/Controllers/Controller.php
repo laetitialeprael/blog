2104e9878443c2ea0530f1bf6ca1d970bafd7502
laetitialeprael <76735358+laetitialeprael@users.noreply.github.com>
2021-09-15 14:14:17 +0200

    Clean Controller.php

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -4,11 +4,11 @@ namespace Src\Controllers;
 
 use Src\Database;
 
-/*
+/**
  * Class Controller
  *
  * @package Src
-*/
+ */
 class Controller
 {
     public const DB_NAME = 'openclassroomsblog';
@@ -19,6 +19,11 @@ class Controller
 
     private static $database;
 
+    /**
+     * Méthode pour se connecter à la base de donnée
+     *
+     * @return void
+     */
     public function getDatabase()
     {
         if (self::$database === null) {
@@ -26,6 +31,12 @@ class Controller
         }
         return self::$database;
     }
+
+    /**
+     * Méthode pour vérifier l'email dans la variable $_SESSION
+     *
+     * @return void
+     */
     public function isAuth()
     {
         if (!isset($_SESSION['email'])) {
